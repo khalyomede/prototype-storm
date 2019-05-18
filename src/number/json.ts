88ea9bc6c50d652c3352691dd15e50a5e8604470
Khalyomede <khalyomede@gmail.com>
2019-05-18 22:56:50 +0200

    added missing doc

--- src/number/json.ts
+++ src/number/json.ts
@@ -1,5 +1,13 @@
 export default (() => {
 	if (!("json" in Number)) {
+		/**
+		 * @description Returns true if the number is greater or equal to 0, else returns false.
+		 * @return {String}
+		 * @example
+		 * const number = 42;
+		 * number.json(); // "42"
+		 * @see [CDN link](https://unpkg.com/@khalyomede/prototype-storm@0.1.0/dist/number/json.js)
+		 */
 		Number.prototype.json = function(): String {
 			return JSON.stringify(this);
 		};
