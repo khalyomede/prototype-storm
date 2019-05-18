e35ad12ca55520dec1468c1b708a54636224fb18
Khalyomede <khalyomede@gmail.com>
2019-05-18 22:55:05 +0200

    added missing return statement

--- src/array/unduplicate.ts
+++ src/array/unduplicate.ts
@@ -2,12 +2,13 @@ export default (() => {
 	if (!("unduplicate" in Array)) {
 		/**
 		 * @description Returns the array and removes any duplicates.
+		 * @return {Array}
 		 * @example
 		 * const array = [1, 1, 2, 3, 3, 0, -1];
 		 * array.unduplicate(); // [1, 2, 3, 0, -1]
 		 * @see [CDN link](https://unpkg.com/@khalyomede/prototype-storm@0.1.0/dist/array/unduplicate.js)
 		 */
-		Array.prototype.unduplicate = function() {
+		Array.prototype.unduplicate = function(): Array {
 			return Array.from(new Set(this));
 		};
 	}
